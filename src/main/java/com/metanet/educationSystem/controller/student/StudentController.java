275360e4fc18abee2ffc29647f4401a205302f78
FIRST_NAME LAST_NAME <dbxodnsms@gmail.com>
2021-09-20 10:44:34 +0900

    .
    .

--- src/main/java/com/metanet/educationSystem/controller/student/StudentController.java
+++ src/main/java/com/metanet/educationSystem/controller/student/StudentController.java
@@ -13,10 +13,17 @@ public class StudentController {
 	@Autowired
 	private StudentService studentService;
 	
-	@RequestMapping("/studentApplyClass")
+	@RequestMapping("/student/studentApplyClass")
 	public String studentApplyClass(Model model) throws Exception {
+		
+		// 1. 파라미터가 (있으면) 받기
+		// 2. 로직 실행 (로직 실행 결과를 Model 에 담기 위해서)
+		// 3. Model에 로직 실행한 결과를 담아서
+		// 4. 뷰로 모델을 보내주는거에요.
+		System.out.println("가지나가지나");
 		model.addAttribute("memberVO", studentService.getMemberVO());
 		model.addAttribute("classVOList",studentService.getClassList());
+		
 		return "student/StudentApplyClassPage";
 	}
 	
