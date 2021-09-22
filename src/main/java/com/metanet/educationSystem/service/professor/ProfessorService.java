15739798f2f78ab6e4395491cd97f80b4ad0aeab
coghks1229 <66114481+coghks1229@users.noreply.github.com>
2021-09-22 17:27:13 +0900

    0922
    studentlist, input student score modal

--- src/main/java/com/metanet/educationSystem/service/professor/ProfessorService.java
+++ src/main/java/com/metanet/educationSystem/service/professor/ProfessorService.java
@@ -5,12 +5,19 @@ import java.util.List;
 
 import com.metanet.educationSystem.model.ClassVO;
 import com.metanet.educationSystem.model.MemberVO;
+import com.metanet.educationSystem.model.ScoreVO;
 
 public interface ProfessorService {
 	
 	public ClassVO getMemberVO() throws Exception;
+	
+	public ScoreVO inputScoreVO() throws Exception;
 
 	public List<ClassVO> professorGetClassList(String professorNO) throws Exception;
+
+	public List<HashMap<String, Object>> professorGetStudentList(String classNO) throws Exception;
+
+	public List<ScoreVO> inputStudentScore(String studentNO) throws Exception;
 	
 	
 
