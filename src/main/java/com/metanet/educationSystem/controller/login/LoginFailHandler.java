8e6d20cc15673240dcb56c4940f6a8f2f0f3e91a
FIRST_NAME LAST_NAME <dbxodnsms@gmail.com>
2021-09-23 10:31:18 +0900

    로그인 다함
    .

--- src/main/java/com/metanet/educationSystem/controller/login/LoginFailHandler.java
+++ src/main/java/com/metanet/educationSystem/controller/login/LoginFailHandler.java
@@ -2,17 +2,19 @@ package com.metanet.educationSystem.controller.login;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -21,37 +23,32 @@ import lombok.Setter;
 @Setter
 public class LoginFailHandler implements AuthenticationFailureHandler {
 
-//	private String memberNO;
-//	private String memberPassword;
-//	private String errormsgname;
-//	private String defaultFailureUrl;
-
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException exception) throws IOException, ServletException {
 		System.out.println("Login Fail Handler");
 
-		String memberNO = request.getParameter("memberNO");
-		String memberPassword = request.getParameter("memberPassword");
-		String errormsg = null;
-		String defaultFailureUrl = "/checkLogin";
-
-		if (exception instanceof InternalAuthenticationServiceException) {
-			errormsg = "0"; // 아이디 잘못 입력
-		} else if (exception instanceof BadCredentialsException) {
-			errormsg = "1"; // 비밀번호를 잘못 입력했습니다.
-		} else if (exception instanceof UsernameNotFoundException) {
-			errormsg = "2"; // 계정이 존재하지 않습니다.
-		} else if (exception instanceof DisabledException) {
-			errormsg = "3"; // 이메일 인증을 해주세요.
-		} else if (exception instanceof SessionAuthenticationException) {
-			errormsg = "4"; // 중복 로그인
+		if (exception instanceof AuthenticationServiceException) {
+			request.setAttribute("loginFailMsg", "존재하지 않는 사용자입니다.");
+		
+		} else if(exception instanceof BadCredentialsException) {
+			request.setAttribute("loginFailMsg", "아이디 또는 비밀번호가 틀립니다.");
+			
+		} else if(exception instanceof LockedException) {
+			request.setAttribute("loginFailMsg", "잠긴 계정입니다..");
+			
+		} else if(exception instanceof DisabledException) {
+			request.setAttribute("loginFailMsg", "비활성화된 계정입니다..");
+			
+		} else if(exception instanceof AccountExpiredException) {
+			request.setAttribute("loginFailMsg", "만료된 계정입니다..");
+			
+		} else if(exception instanceof CredentialsExpiredException) {
+			request.setAttribute("loginFailMsg", "비밀번호가 만료되었습니다.");
 		}
 		
-		System.out.println("입력 ID : "+memberNO+"\n입력 패스워드 : " + memberPassword + "\nerrormsg = " + exception);
-		request.setAttribute("errormsg", errormsg);
-        
-		request.getRequestDispatcher(defaultFailureUrl).forward(request, response);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/checkLogin");
+		dispatcher.forward(request, response);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/login/LoginPage.jsp
+++ src/main/webapp/WEB-INF/views/login/LoginPage.jsp
@@ -1,31 +1,41 @@
 <%@ page language="java" contentType="text/html; charset=EUC-KR" pageEncoding="UTF-8"%>
+<jsp:include page="/incl/Head.jsp" />
 <!DOCTYPE html>
-<html>
+<html class="h-100" lang="en">
 
 <head>
-<jsp:include page="/incl/Head.jsp" />
-<title>loginPage</title>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript">
-/* $(document).ready(function(){
-		$("#doLogin").click(function(){
-			var queryString = $("form[name=loginInfo]").serialize();
-			$.ajax({
-				url: "/doLoginImpl",
-				type: "post",
-				data:queryString,
-				success: function(data){
-					alert("오류 번호 : ${errormsg}");
-				},
-				error: function(request, status, error){
-					alert("아이디 또는 비밀번호 오류입니다.");
-				}
-				})
-			})
-		}) */
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<title>Quixlab - Bootstrap Admin Dashboard Template by Themefisher.com</title>
+<!-- Favicon icon -->
+<link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script>
 </script>
 </head>
+
 <body class="h-100">
+
+	<!--*******************
+        Preloader start
+    ********************-->
+	<div id="preloader">
+		<div class="loader">
+			<svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+            </svg>
+		</div>
+	</div>
+	<!--*******************
+        Preloader end
+    ********************-->
+
+
+
+
+
 	<div class="login-form-bg h-100">
 		<div class="container h-100">
 			<div class="row justify-content-center h-100">
@@ -34,18 +44,19 @@
 						<div class="card login-form mb-0">
 							<div class="card-body pt-5">
 								<a class="text-center">
-									<h4>Login</h4>
+									<h4>로그인</h4>
 								</a>
 
-								<form class="mt-5 mb-5 login-input" name="loginInfo" action="/doLogin" method="post">
+								<form class="mt-5 mb-5 login-input" action="/doLogin" method="post">
 									<div class="form-group">
 										<input type="text" class="form-control" placeholder="Id" name="memberNO">
 									</div>
 									<div class="form-group">
 										<input type="password" class="form-control" placeholder="Password" name="memberPassword">
 									</div>
-									<button type="submit" class="btn login-form__btn submit w-100" id="doLogin">Sign In</button>
+									<button class="btn login-form__btn submit w-100" type="submit" onclick="doLogin();">Sign In</button>
 								</form>
+								<p class="mt-5 login-form__footer" align="center" style="color:blue;">${requestScope.loginFailMsg}</p>
 							</div>
 						</div>
 					</div>
@@ -53,6 +64,8 @@
 			</div>
 		</div>
 	</div>
+
+
 
 	<footer>
 		<jsp:include page="/incl/Footer.jsp" />
