f808ad0985ef8e9c15177e3863521f57e9ed5895
FIRST_NAME LAST_NAME <dbxodnsms@gmail.com>
2021-09-29 15:26:53 +0900

    오타수정
    .

--- src/main/java/com/metanet/educationSystem/service/admin/AdminServiceImpl.java
+++ src/main/java/com/metanet/educationSystem/service/admin/AdminServiceImpl.java
@@ -97,26 +97,24 @@ public class AdminServiceImpl implements AdminService {
 
 	@Override
 	public void adminDeleteClass(String classNO) throws Exception {
-
 		this.adminMapper.adminDeleteClass(classNO);
-
 	}
 
 	@Override
 	public void studentInsert(StudentVO studentVO) throws Exception {
 		this.adminMapper.studentInsert(studentVO);
-		
+
 	}
 
 	@Override
 	public void professorInsert(ProfessorVO professorVO) throws Exception {
 		this.adminMapper.professorInsert(professorVO);
-		
+
 	}
 
 	@Override
 	public List<String> getProfessorNOList() throws Exception {
-		
+
 		return this.adminMapper.getProfessorNOList();
 	}
 
@@ -124,6 +122,7 @@ public class AdminServiceImpl implements AdminService {
 	public String getClassNO() throws Exception {
 		// TODO Auto-generated method stub
 		return this.adminMapper.getClassNO();
+	}
 
 	@Override
 	public HashMap<String, Object> getAdminInfo(String memberNO) {
